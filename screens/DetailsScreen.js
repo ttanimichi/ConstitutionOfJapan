2f6ff2fb5caa9b1feedecbdf2942be53aa81cacb
Tsukuru Tanimichi <info+git@ttanimichi.com>
2019-01-14 04:08:36 +0900

    Home and Details

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-elements'
+import details from '../data/details';
 
-export default function DetailsScreen() {
+export default function DetailsScreen({ navigation }) {
+  const key = navigation.getParam('key', 'no-key');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>詳細画面</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={[styles.text, styles.textBold]}>{key}</Text>
+      </View>
+
+      <Divider />
+
+      <View style={styles.header}>
+        <Text style={styles.text}>{details[key]}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 18,
+    paddingRight: 18,
+  },
+  text: {
+    fontSize: 18,
+    lineHeight: 26
+  },
+  textBold: {
+    fontWeight: 'bold'
+  },
+  header: {
+    marginBottom: 8,
+    marginTop: 24
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SectionList, StyleSheet } from 'react-native';
+import { ListItem } from 'react-native-elements'
+
+import sections from '../data/sections';
 
 export default function HomeScreen({ navigation }) {
+  const item = ({ item }) => (
+    <ListItem
+      title={item}
+      key={item}
+      onPress={() => navigation.navigate('Details', { key: item })}
+    />
+  );
+
+  const sectionHeader = ({ section }) => (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>{section.title}</Text>
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text onPress={() => navigation.navigate('Details', { id: 42 })}>
-        詳細を表示
-      </Text>
+    <View>
+      <SectionList
+        renderItem={item}
+        renderSectionHeader={sectionHeader}
+        sections={sections}
+        keyExtractor={(item, index) => item + index}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.12)',
+    backgroundColor: '#efefef',
+  },
+  sectionTitle: {
+    color: 'black',
+    fontSize: 14,
+    marginBottom: 8,
+    marginLeft: 18,
+    marginRight: 18,
+    marginTop: 24,
+    opacity: 0.8,
+  },
+});
